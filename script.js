5ed6ec038a3bd2a782794186ec51bfb58a519102
attila <attilavigh.contact@gmail.com>
2021-06-21 11:48:41 +0100

    Ex: 3) Create a function to remove a character at the specified position of a given string and return the new string.

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ console.log( checkTwoNumbersAre50( 23, 3));
 console.log( checkTwoNumbersAre50( 25, 25));
 console.log( checkTwoNumbersAre50( 50, 25));
 console.log( checkTwoNumbersAre50(  5, 50));
+
+// 3) Create a function to remove a character at the specified position of a given string and return the new string.
+
+
+const extractChPosistion = ( string, position) => {
+    console.log("position: ", position);
+    if (position === undefined || position > string.length || position < -string.length) {
+        return " You have to introduce a valid number "
+    }
+    else {
+        return string.substr(0, position) + string.substr( position +1)
+    }
+
+}
+
+let string = "Hello world!"
+
+console.log(  extractChPosistion( string, -1) );
+console.log(  extractChPosistion( string, -2) );
+console.log(  extractChPosistion( string, -3) );
+console.log(  extractChPosistion( string, -11) );
+console.log(  extractChPosistion( string, -string.length) );
+console.log(  extractChPosistion( string, -13) );
+console.log(  extractChPosistion( string, -14) );
+console.log(  extractChPosistion( string, 0) );
+console.log(  extractChPosistion( string, 1) );
+console.log(  extractChPosistion( string, 2) );
+console.log(  extractChPosistion( string, 11) );
+console.log(  extractChPosistion( string, string.length) );
+console.log(  extractChPosistion( string, 13) );
+console.log(  extractChPosistion( string) );
